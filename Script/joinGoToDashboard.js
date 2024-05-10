d10563bcab78b9fdfd6b362dd39f597e4fe5f006
Larebdrigh1 <llareb@uef.fi>
2024-05-10 22:24:47 +0300

    Update joinGoToDashboard.js

--- Script/joinGoToDashboard.js
+++ Script/joinGoToDashboard.js
@@ -1,3 +1,3 @@
 document.getElementById('joinTeamBtn').addEventListener('click', function() {
-  window.location.href = '../Pages/dashboard.php';
+  window.location.href = '../Pages/dashboard.html';
 });
