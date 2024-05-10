0a18f1afc28da6a92c136cc58d096781d76a0521
lareb <llareb@uef.fi>
2024-05-11 01:31:02 +0500

    fixed redirection issue

--- Database/db.php
+++ Database/db.php
@@ -8,14 +8,4 @@ catch (PDOException $e) {
     print("Error connecting to SQL Server.");
     die(print_r($e));
 }
-
-// SQL Server Extension Sample Code:
-// $connectionInfo = array("UID" => "lareb", "pwd" => "Hahahahaha55", "Database" => "sportclub", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-// $serverName = "tcp:lareb.database.windows.net,1433";
-// $conn = sqlsrv_connect($serverName, $connectionInfo);
-
-
-// print_r($conn);
 ?>
-
-
